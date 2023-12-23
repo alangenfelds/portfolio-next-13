200ac49e2a36d5b00429c154423b733563fbc881
arturs.langenfelds@28stone.com <alangenfeld@gmail.com>
2023-12-23 11:25:47 +0200

    updated about section

--- components/about.tsx
+++ components/about.tsx
@@ -28,18 +28,19 @@ const About = () => {
         <span className="italic">My favorite part of programming</span> is the
         problem-solving aspect. I <span className="underline">love</span> the
         feeling of finally figuring out a solution to a problem. My core stack
-        is <span className="font-medium">React, Next.js, TypeScript</span>. I am
-        also familiar with Vue.js and Svelte. I am always looking to learn new
+        is{" "}
+        <span className="font-medium">React, Next.js, TypeScript, Node.js</span>
+        . I am also familiar with MERN Stack. I am always looking to learn new
         technologies. I am opened for a{" "}
-        <span className="font-medium">full-time position</span> as a frontend
-        developer.
+        <span className="font-medium">full-time position</span> as a Frontend or
+        Full Stack developer (React/Node.js).
       </p>
 
       <p>
         <span className="italic">When I'm not coding</span>, I enjoy playing
         video games, watching movies, and going to hiking trips. I also enjoy{" "}
         <span className="font-medium">learning new things</span>. I am currently
-        learning about <span className="font-medium">Web3</span>.
+        preparing for <span className="font-medium">AWS</span> certification.
       </p>
     </motion.section>
   );
